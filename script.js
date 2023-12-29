fca037cae1494632506b836640e2baae93227a44
SHANIB C K <ckshanib8@gmail.com>
2023-12-29 16:05:36 +0530

    language select option code added at script.js file

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let speech = new SpeechSynthesisUtterance();
 
+let voices = [];
+
+let voiceSelector = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
+
+  voices.forEach(
+    (voice, i) => (voiceSelector.options[i] = new Option(voice.name, i))
+  );
+};
+
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
